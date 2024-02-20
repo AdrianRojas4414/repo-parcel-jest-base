6e77caf2df397ad1f3d530343b417ca40c442daa
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-02-20 00:12:52 -0400

    Saludador con idioma completado

--- src/presenter.js
+++ src/presenter.js
@@ -5,6 +5,7 @@ import saludar from "./saludador";
 const nombre_input = document.querySelector("#nombre");
 const edad_input = document.querySelector("#edad");
 const genero_input = document.querySelector("#genero");
+const idioma_input = document.querySelector("#idioma");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -14,10 +15,11 @@ form.addEventListener("submit", (event) => {
   const nombre = nombre_input.value;
   const edad = edad_input.value; //agarra el valor del index
   const genero = genero_input.value;
+  const idiom = idioma_input.value; //implementando el idioma
 
   //implementando el tiempo del sistema
   const fecha = new Date();
   const hora = fecha.getHours();
 
-  div.innerHTML = "<p>" + saludar(nombre, edad, genero, hora) + "</p>";  
+  div.innerHTML = "<p>" + saludar(nombre, edad, genero, hora, idiom) + "</p>";  
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,50 +1,114 @@
-function saludar(nom, edad, gene, hora) {
-  if(hora >=5 && hora<=11){
-    if(edad > 30){
-      if(gene == "M"){
-        return "Buenos dias Sr. " + nom;
-      }else if(gene == "F"){
-        return "Buenos dias Sra. " + nom;
-      }
-    }else{
-      if(gene == "M"){
-        return "Buenos dias joven " + nom;
-      }else if(gene == "F"){
-        return "Buenos dias seniorita " + nom;
-      }
-    }
-
-  }else{
-    if(hora >=12 && hora<=18){
+function saludar(nom, edad, gene, hora, idi) {
+  if(idi == "ES"){
+    if(hora >=5 && hora<=11){
       if(edad > 30){
         if(gene == "M"){
-          return "Buenas tardes Sr. " + nom;
+          return "Buenos dias Sr. " + nom;
         }else if(gene == "F"){
-          return "Buenas tardes Sra. " + nom;
+          return "Buenos dias Sra. " + nom;
         }
       }else{
         if(gene == "M"){
-          return "Buenas tardes joven " + nom;
+          return "Buenos dias joven " + nom;
         }else if(gene == "F"){
-          return "Buenas tardes seniorita " + nom;
+          return "Buenos dias seniorita " + nom;
         }
       }
+
     }else{
+      if(hora >=12 && hora<=18){
+        if(edad > 30){
+          if(gene == "M"){
+            return "Buenas tardes Sr. " + nom;
+          }else if(gene == "F"){
+            return "Buenas tardes Sra. " + nom;
+          }
+        }else{
+          if(gene == "M"){
+            return "Buenas tardes joven " + nom;
+          }else if(gene == "F"){
+            return "Buenas tardes seniorita " + nom;
+          }
+        }
+  
+  
+      }else{
+        if(edad > 30){
+          if(gene == "M"){
+             return "Buenas noches Sr. " + nom;
+          }else if(gene == "F"){
+             return "Buenas noches Sra. " + nom;
+           }
+         }else{
+          if(gene == "M"){
+            return "Buenas noches joven " + nom;
+          }else if(gene == "F"){
+            return "Buenas noches seniorita " + nom;
+           }
+        }
+      }
+    }
+
+
+
+
+  }else if(idi == "EN"){
+if(hora >=5 && hora<=11){
       if(edad > 30){
         if(gene == "M"){
-           return "Buenas noches Sr. " + nom;
+          return "Good morning Mr. " + nom;
         }else if(gene == "F"){
-           return "Buenas noches Sra. " + nom;
-         }
-       }else{
+          return "Good morning Ms. " + nom;
+        }
+      }else{
         if(gene == "M"){
-          return "Buenas noches joven " + nom;
+          return "Good morning young " + nom;
         }else if(gene == "F"){
-          return "Buenas noches seniorita " + nom;
-         }
+          return "Good morning young " + nom;
+        }
+      }
+  
+    }else{
+      if(hora >=12 && hora<=18){
+        if(edad > 30){
+          if(gene == "M"){
+            return "Good afternoon Mr. " + nom;
+          }else if(gene == "F"){
+            return "Good afternoon Ms. " + nom;
+          }
+        }else{
+          if(gene == "M"){
+            return "Good afternoon young " + nom;
+          }else if(gene == "F"){
+            return "Good afternoon young " + nom;
+          }
+        }
+  
+  
+      }else{
+        if(edad > 30){
+          if(gene == "M"){
+             return "Good evening Mr. " + nom;
+          }else if(gene == "F"){
+             return "Good evening Ms. " + nom;
+           }
+         }else{
+          if(gene == "M"){
+            return "Good evening young " + nom;
+          }else if(gene == "F"){
+            return "Good evening young " + nom;
+           }
+        }
       }
     }
   }
+
+
+  
+
+
+
+
 }
   
   export default saludar;
