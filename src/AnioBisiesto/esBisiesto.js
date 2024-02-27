fc3fdbcd1a8932ecc6f9f90fe40016a60fa55393
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-02-27 01:02:04 -0400

    inicio del segundo ciclo

--- src/AnioBisiesto/esBisiesto.js
+++ src/AnioBisiesto/esBisiesto.js
@@ -1,5 +1,9 @@
 function bisiesto(n){
-    
+    if(n % 400 == 0){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 export default bisiesto;
--- src/AnioBisiesto/esBisiesto.test.js
+++ src/AnioBisiesto/esBisiesto.test.js
@@ -1,7 +1,11 @@
 import bisiesto from "./esBisiesto";
 
-describe("Generar un Fizz Buzz",() =>{
+describe("Es Bisiesto",() =>{
     it("Todos los años divisibles por  400 SON años bisiestos",() =>{
         expect(bisiesto(4800)).toEqual(true);
+    })
+
+    it("Todos los años que no son divisibles por 400 NO SON años bisiestos",() =>{
+        expect(bisiesto(4800)).toEqual(false);
     })
 })
