cf659025e9f15f1be0505d9d409c36e3e5cf5c67
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-02-27 00:07:25 -0400

    Prueba del FizzBuzz

---
+++ src/Buzz/FizzBuzz.test.js
@@ -0,0 +1,7 @@
+import generarFizzBuzz from "./FizzBuzz";
+
+describe("Generar un Fizz Buzz",() =>{
+    it("Debe devolver el mismo numero que se le manda",() =>{
+        expect(generarFizzBuzz(1))
+    })
+})
