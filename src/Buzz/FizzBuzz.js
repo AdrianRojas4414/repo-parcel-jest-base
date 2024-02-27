978df73ea361963e54f42dfc9cbc2dd87005796b
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-02-27 00:42:27 -0400

    FizzBuzz Completado

--- src/Buzz/FizzBuzz.js
+++ src/Buzz/FizzBuzz.js
@@ -5,15 +5,25 @@ function generarFizzBuzz(n){
     if(n == 5){
         return "Buzz";
     }
+    if(n != 3 && n != 5){
+        let result = [];
+        for (let i = 1; i<=n; i++){
+            if(i%3 == 0 && i%5 == 0){
+                result.push("FizzBuzz");
+            }else if(i%3 == 0){
+                result.push("Fizz");
+            }else if(i%5 == 0){
+                result.push("Buzz");
+            }else{
+                result.push(i.toString());
+            }
+        }
+        return result.join(', ');
+    }
 }
 
-/*let result = [];
-    for (let i = 1; i<=n; i++){
-        if(i%3 == 0){
-            result.push("Fizz");
-        }else{
-            result.push(i.toString());
-        }
-    } */
+
+    
+
 
 export default generarFizzBuzz;
--- src/Buzz/FizzBuzz.test.js
+++ src/Buzz/FizzBuzz.test.js
@@ -2,10 +2,14 @@ import generarFizzBuzz from "./FizzBuzz";
 
 describe("Generar un Fizz Buzz",() =>{
     it("Si el número es 3 o múltiplo de 3 se imprime Fizz",() =>{
-        expect(generarFizzBuzz(3)).toEqual("Fizz")
+        expect(generarFizzBuzz(3)).toEqual("Fizz");
     })
 
     it("Si el número es 5 o múltiplo de 5 se imprime Buzz",() =>{
-        expect(generarFizzBuzz(5)).toEqual("Buzz")
+        expect(generarFizzBuzz(5)).toEqual("Buzz");
+    })
+
+    it("Si el número es múltiplo de 3 y 5 se imprime FizzBuzz",() =>{
+        expect(generarFizzBuzz(15)).toEqual("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz");
     })
 })
