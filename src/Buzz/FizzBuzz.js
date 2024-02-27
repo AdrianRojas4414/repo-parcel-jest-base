c5bbafbd06ab7975ac40e7959ed6f0f454741714
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-02-27 00:23:58 -0400

    paso la primera prueba

--- src/Buzz/FizzBuzz.js
+++ src/Buzz/FizzBuzz.js
@@ -1,5 +1,13 @@
 function generarFizzBuzz(n){
-    return "1";
+    let result = [];
+    for (let i = 1; i<=n; i++){
+        if(i%3 == 0){
+            result.push("Fizz");
+        }else{
+            result.push(i.toString());
+        }
+    }
+    return result.join(', ');
 }
 
 export default generarFizzBuzz;
