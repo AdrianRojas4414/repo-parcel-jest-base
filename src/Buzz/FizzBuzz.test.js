ae0c8f86c70aa8afe914c0c6e278e8335b21907a
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-02-27 00:14:27 -0400

    Primer ajuste en el test antes de cambiar el codigo

--- src/Buzz/FizzBuzz.test.js
+++ src/Buzz/FizzBuzz.test.js
@@ -2,6 +2,10 @@ import generarFizzBuzz from "./FizzBuzz";
 
 describe("Generar un Fizz Buzz",() =>{
     it("Debe devolver el mismo numero que se le manda",() =>{
-        expect(generarFizzBuzz(1))
+        expect(generarFizzBuzz(1)).toEqual("1")
+    })
+
+    it("Si el número es 3 o múltiplo de 3 se imprime Fizz",() =>{
+        expect(generarFizzBuzz(10)).toEqual("1, 2, Fizz, 4, 5, Fizz, 7, 8, Fizz, 10")
     })
 })
