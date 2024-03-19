e021c5ba2aa59672862996dc74b3e80c44721f3a
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-03-19 01:51:33 -0400

    Refactoring de los metodos de la clase, se aniadio una funcion para el Spare y se hizo una prueba exitosa: Se hace un spare y se caen 3 pinos en el siguiente lanzamiento y deberia devolver 16 en el score

--- src/BowlingGame/BowlGame.js
+++ src/BowlingGame/BowlGame.js
@@ -10,16 +10,27 @@ class Game{
     }
 
     resetRolls(){
-        this.rolls = new Array(21).fill(0);
+        this.rolls.fill(0);
         this.currentRoll = 0;
     }
 
     Score(){
         let score = 0;
-        for(let i=0; i<this.rolls.length; i++){
-            score = score + this.rolls[i];
+        let frameIndex = 1;
+        for(let frame=0; frame<10; frame++){
+            if(this.isSpare(frameIndex)){
+                score = score + 10 + this.rolls[frameIndex + 2];
+                frameIndex = frameIndex + 2;
+            }else{
+                score = score + this.rolls[frameIndex] + this.rolls[frameIndex + 1]; // se suman los dos lanzamientos que no suman 10
+                frameIndex = frameIndex + 2;
+            }
         }
         return score;
+    }
+
+    isSpare(frameIndex){
+        return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10; //se suman los dos lanzamientos para ver si suman 10
     }
 }
 
--- src/BowlingGame/Bowlgame.test.js
+++ src/BowlingGame/Bowlgame.test.js
@@ -2,6 +2,17 @@ const Game = require("./BowlGame");
 
 let g = new Game();
 
+function rollMany(n, pins){
+    for(let i=0; i<n; i++){
+        g.roll(pins);
+    }
+}
+
+function rollSpare(){
+    g.roll(5);
+    g.roll(5);
+}
+
 describe("Bowling Game",() =>{
     it("Se envia 0 a roll y deberia devolver 0 en el score",() =>{
         rollMany(20, 0);
@@ -14,19 +25,13 @@ describe("Bowling Game",() =>{
         expect(g.Score()).toEqual(20);
     })
 
-    it("Se envia 20 puntos a roll y deberia devolver 20 en el score",() =>{
+    it("Se hace un spare y se caen 3 pinos en el siguiente lanzamiento y deberia devolver 16 en el score",() =>{
         g.resetRolls();
-        g.roll(5);
-        g.roll(5);  // se realiza un spare
-        g.roll(3);
+        rollSpare();  // se realiza un spare
+        g.roll(3);   // en el siguiente lanzamiento se tiran 3 pinos
         rollMany(17, 0);
         expect(g.Score()).toEqual(16);
     })
 
 })
 
-function rollMany(n, pins){
-    for(let i=0; i<n; i++){
-        g.roll(pins);
-    }
-}
