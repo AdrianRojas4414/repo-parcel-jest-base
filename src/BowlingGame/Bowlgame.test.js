1f66c4d19a5c59a516bf108f8fdc87db010161df
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-03-19 00:08:38 -0400

    Refactoring: se aniadio la funcion rollMany, se declara la clase Game antes y se modificaron los its para mayor dinamismo del codigo

--- src/BowlingGame/Bowlgame.test.js
+++ src/BowlingGame/Bowlgame.test.js
@@ -1,25 +1,22 @@
 const Game = require("./BowlGame");
 
+let g = new Game();
+
 describe("Bowling Game",() =>{
     it("Se envia 0 a roll y deberia devolver 0 en el score",() =>{
-
-        let g = new Game();
-
-        for(let i=0; i<20; i++){
-            g.roll(0);
-        }
-
+        rollMany(20, 0);
         expect(g.Score()).toEqual(0);
     })
 
     it("Se envia 20 puntos a roll y deberia devolver 20 en el score",() =>{
-
-        let g = new Game();
-
-        for(let i=0; i<20; i++){
-            g.roll(1);
-        }
-
+        rollMany(20, 1);
         expect(g.Score()).toEqual(20);
     })
+
 })
+
+function rollMany(n, pins){
+    for(let i=0; i<n; i++){
+        g.roll(pins);
+    }
+}
